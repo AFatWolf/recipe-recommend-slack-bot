26cc42f2b9fceb5d97ea00c21ffcacde21ef6032
Quan Ha <s1f101910027@iniad.org>
2020-07-16 20:07:04 +0900

    Add get recipe list function

--- team/cookpad.py
+++ team/cookpad.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
 def sendCookpadRequest(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
@@ -15,4 +16,10 @@ def searchCookpad(self, ingredientsLst):
         htmlContent = "Unable to make GET request"
     return htmlContent
 
-
+def getRecipeLst(self, htmlContent):
+    lst = []
+    html = bs(htmlContent, 'html.parser')
+    recipe_list = html.find_all("a", "flex items-center")
+    for r in recipe_list:
+        lst.append(r.get('href'))
+    return lst    
